8542d735ed40aa06bef1379d97262ed50b76b913
Atiqur Rahman <atiqur.india@gmail.com>
2022-04-09 00:46:07 +0530

    Test Stake Tokens

--- tests/unit/test_token_farm.py
+++ tests/unit/test_token_farm.py
@@ -26,3 +26,22 @@ def test_set_price_feed_contract():
         token_farm.setPriceFeedContract(
             dapp_token.address, price_feed_address, {"from": non_account}
         )
+
+
+def test_stake_tokens(amount_staked):
+    # Arrange
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("Only for local testing!")
+    account = get_account()
+    token_farm, dapp_token = deploy_token_farm_and_dapp_token()
+    # Act
+    dapp_token.approve(token_farm.address, amount_staked, {"from": account})
+    token_farm.stakeTokens(amount_staked, dapp_token.address, {"from": account})
+    # Assert
+    assert (
+        token_farm.stakingBalance(dapp_token.address, account.address) == amount_staked
+    )
+
+
+def test_issue_tokens():
+    pass
