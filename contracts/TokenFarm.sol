8994ab84b8196c8bbcf28bb4e63de406ea18b9c8
Atiqur Rahman <atiqur.india@gmail.com>
2022-04-08 16:32:37 +0530

    Stake tokens

--- contracts/TokenFarm.sol
+++ contracts/TokenFarm.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 import '@openzeppelin/contracts/access/Ownable.sol';
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 
 contract TokenFarm is Ownable {
     // stakeToken
@@ -9,13 +10,17 @@ contract TokenFarm is Ownable {
     // issueTokens
     // addAllowedTokens
     // getEthValue
+    // mapping token address -> staker address -> amount
+    mapping(address => mapping(address => uint256)) public stakingBalance;
     address[] public allowedTokens;
 
     function stakeToken(uint256 _amount, address _token) public {
-        // what token can they stake?
-        // how much can they stake?
         require(_amount > 0, 'Amount must be more than 0');
-        // require(_token is allowed???)
+        require(tokenIsAllowed(_token), 'Token is currently not allowed');
+        IERC20(_token).transferFrom(msg.sender, address(this), _amount);
+        stakingBalance[_token][msg.sender] =
+            stakingBalance[_token][msg.sender] +
+            _amount;
     }
 
     function addAllowedTokens(address _token) public onlyOwner {
