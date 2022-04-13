d40e789266dc6c67d1806a862061ab2e4251230b
Atiqur Rahman <atiqur.india@gmail.com>
2022-04-13 22:38:38 +0530

    Basic styling

--- front_end/src/components/yourWallet/YourWallet.tsx
+++ front_end/src/components/yourWallet/YourWallet.tsx
@@ -1,5 +1,5 @@
 import { Token } from '../Main';
-import { Box, Tab } from '@material-ui/core';
+import { Box, Tab, makeStyles } from '@material-ui/core';
 import { TabContext, TabList, TabPanel } from '@material-ui/lab';
 import React, { useState } from 'react';
 import { WalletBalance } from './WalletBalance';
@@ -9,17 +9,33 @@ interface YourWalletProps {
   supportedTokens: Array<Token>;
 }
 
+const useStyles = makeStyles((theme) => ({
+  tabContent: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    gap: theme.spacing(4),
+  },
+  box: {
+    backgroundColor: 'white',
+    borderRadius: '25px',
+  },
+  header: {
+    color: 'white',
+  },
+}));
+
 export const YourWallet = ({ supportedTokens }: YourWalletProps) => {
   const [selectedTokenIndex, setSelectedTokenIndex] = useState<number>(0);
 
   const handleChange = (event: React.ChangeEvent<{}>, newValue: string) => {
     setSelectedTokenIndex(parseInt(newValue));
   };
-
+  const classes = useStyles();
   return (
     <Box>
-      <h1>Your Wallet!</h1>
-      <Box>
+      <h1 className={classes.header}>Your Wallet!</h1>
+      <Box className={classes.box}>
         <TabContext value={selectedTokenIndex.toString()}>
           <TabList onChange={handleChange} aria-label='stake form tabs'>
             {supportedTokens.map((token, index) => {
@@ -31,7 +47,7 @@ export const YourWallet = ({ supportedTokens }: YourWalletProps) => {
           {supportedTokens.map((token, index) => {
             return (
               <TabPanel value={index.toString()} key={index}>
-                <div>
+                <div className={classes.tabContent}>
                   <WalletBalance token={supportedTokens[selectedTokenIndex]} />
                   <StakeForm token={supportedTokens[selectedTokenIndex]} />
                 </div>
